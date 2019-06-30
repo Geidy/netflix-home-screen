dbe7156c065873daf04697f8a1084dd58e40727f
Geidy <geidydm@gmail.com>
2019-06-30 10:51:44 -0400

    started header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
 <Navbar/>
+<Header/>
      here the components will go 
     </div>
   );
